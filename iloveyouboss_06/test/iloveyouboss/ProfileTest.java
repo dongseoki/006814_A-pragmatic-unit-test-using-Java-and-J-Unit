660336831975dfdfc88cc5d18548b780d331dff6
dongseoki <43495319+dongseoki@users.noreply.github.com>
2022-01-28 21:44:05 +0900

    2장 테스트 성공

--- iloveyouboss_06/test/iloveyouboss/ProfileTest.java
+++ iloveyouboss_06/test/iloveyouboss/ProfileTest.java
@@ -1,24 +1,44 @@
-/***
- * Excerpted from "Pragmatic Unit Testing in Java with JUnit",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/utj2 for more book information.
-***/
+
 package iloveyouboss;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.*;
 
 public class ProfileTest {
-
+	Question question ;
+	Profile profile   ;
+	Criteria criteria ;
+	@Before
+	public void create() {
+	      question = new BooleanQuestion(1, "Got bonuses?");
+	      profile   = new Profile("Bull Hockey, Inc.");
+	      criteria = new Criteria();
+	}
+	
    @Test
-   public void test() {
-      Profile profile = new Profile("Bull Hockey, Inc.");
-      Question question = new BooleanQuestion(1, "Got bonuses?");
-      Criteria criteria = new Criteria();
-      Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-      Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-      criteria.add(criterion);
+   public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+
+      profile.add(new Answer(question, Bool.FALSE));
+      criteria.add(new Criterion(new Answer(question, Bool.TRUE), 
+    		  Weight.MustMatch));
+      
+      boolean matches = profile.matches(criteria);
+      assertFalse(matches);
+//      assertTrue(matches);
+   }
+   
+   
+   @Test
+   public void matchAnswersTrueForAnyDontCareCriteria() {
+      profile.add(new Answer(question, Bool.FALSE));
+      
+      criteria.add(new Criterion(new Answer(question, Bool.TRUE),
+    		  Weight.DontCare));
+      
+      boolean matches = profile.matches(criteria);
+//      assertFalse(matches);
+      assertTrue(matches);
    }
 }
